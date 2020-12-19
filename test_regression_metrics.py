6d33ea2af0af08c5f8e2778774788ca460d9903d
Poojitha Gowthaman <pg.poojithagowthaman@gmail.com>
2020-12-18 18:11:58 -0600

    Added exception handling

--- test_regression_metrics.py
+++ test_regression_metrics.py
@@ -6,19 +6,48 @@ import numpy as np
 import random
 import numbers
 
+#user defined exceptions
+class KnnValueError(Exception):
+    pass
+
+class InvalidModel(Exception):
+    pass
+
 class TestRegressionMetrics(unittest.TestCase):
     mpg_to_predict, assert_rmse,assert_mse,assert_mae,assert_mape =0,0,0, 0, 0
-    test_knn_regressor, actual_mpg,predicted_mpg = 0,0, 0
-    def setUpClass():
-        TestRegressionMetrics.test_knn_regressor = knn.KNN("regressor",7)
-        TestRegressionMetrics.test_knn_regressor.load_csv('datasets/auto_mpg.csv','mpg')
-        TestRegressionMetrics.test_knn_regressor.train_test_split(0.25)
+    test_knn_regressor, actual_mpg,predicted_mpg,k = 0,0,0,0
+    model_type = " "
+    
+    @classmethod
+    def setUpClass(cls):
+        try:
+            TestRegressionMetrics.test_knn_regressor = knn.KNN("regressor",7)
+            if TestRegressionMetrics.k < 0 and TestRegressionMetrics.k > 10:
+                raise KnnValueError()
+            if TestRegressionMetrics.model_type!= "regressor" or TestRegressionMetrics.model_type!="classifier":
+                raise InvalidModel()
+        except InvalidModel:
+            print('K nearest neighbors is advisable to a maximum of 10 for statistical effiency')
+        
+        try:
+            TestRegressionMetrics.test_knn_regressor.load_csv('datasets/auto_mpg.csv','mpg')
+        except FileNotFoundError:
+            print('Dataset not found, check the datasets folder')
+            
+        try:
+            TestRegressionMetrics.test_knn_regressor.train_test_split(0.25)
+        except ValueError:
+            print('Enter a value between 0.1 to 0.9 as test-train split percentage')
+        
         print('from setUpClass')
     
     def setUp(self):
-        
-        self.actual_mpg=[19,18,23,28]
-        self.mpg_to_predict=np.array([[6,250,100,3282,15],[6,250,88,3139,14.5],[4,122,86,2220,14],[4,116,90,2123,14]])
+        try:
+            self.actual_mpg=[19,18,23,28]
+            self.mpg_to_predict=np.array([[6,250,100,3282,15],[6,250,88,3139,14.5],[4,122,86,2220,14],[4,116,90,2123,14]])
+        except IndexError:
+            print('Ensure array lengths of actual_mpg and mpg_to_predict match')
+            
         self.predicted_mpg= gp.generate_predictions(TestRegressionMetrics.test_knn_regressor,self.mpg_to_predict,'train')
         
     def test_model_rmse(self):
@@ -55,7 +84,9 @@ class TestRegressionMetrics(unittest.TestCase):
         
     def tearDown(self):
         print('Test Success!!!')
-    def tearDownClass():
+    
+    @classmethod
+    def tearDownClass(cls):
         print("Regressor Assessment: \n RMSE {} MAE: {} MAPE: {} MSE: {}".format(TestRegressionMetrics.assert_rmse,TestRegressionMetrics.assert_mae, 
               TestRegressionMetrics.assert_mape,TestRegressionMetrics.assert_mse))
 unittest.main (argv =[''], verbosity=2, exit= False) 
--- test_train_split.py
+++ test_train_split.py
@@ -3,6 +3,14 @@ from KNN.modelling import KNN_data_collection as knn
 import numpy as np
 # I divided the test cases into classes to show classification and regression. Repeating them for both will be iterative. 
 #So I believe this meets the criteria of TestClass with 2 test cases (Testload_csv,TestVariables ) with 4 asserts. I just differentiated it with 2 TestClasses
+
+#user defined exceptions
+class KnnValueError(Exception):
+    pass
+
+class InvalidModel(Exception):
+    pass
+
 class TestDataCollection_regressor(unittest.TestCase):
     x_train = 0
     x_test = 0
@@ -13,15 +21,29 @@ class TestDataCollection_regressor(unittest.TestCase):
     assert_ytrain = 0
     assert_ytest = 0
     x, y =0, 0
-    test_knn_regressor =0
+    test_knn_regressor,k =0, 0
+    model_type = ""
     def setUpClass():
-        TestDataCollection_regressor.test_knn_regressor = knn.KNN("regressor",2)
+        try:
+            TestDataCollection_regressor.test_knn_regressor = knn.KNN("regressor",2)
+            if TestDataCollection_regressor.k < 0 and TestDataCollection_regressor.k > 10:
+                raise KnnValueError()
+            if TestDataCollection_regressor.model_type!= "regressor" or TestDataCollection_regressor.model_type!="classifier":
+                raise InvalidModel()
+        except KnnValueError:
+            print('K nearest neighbors is advisable to a maximum of 10 for statistical effiency')
+        except InvalidModel:
+            print('Invalid Model type, We are perfoming KNN Regression or KNN Classification')
         
         
     def setUp(self):
         self.test_size = 0.25
-        self.x = np.array([[6,250,100,3282,15],[6,250,88,3139,14.5], [4,122,86,2220,14],[4,116,90,2123,14]])
-        self.y = np.array([12, 45, 11, 90])
+        try:
+            self.x = np.array([[6,250,100,3282,15],[6,250,88,3139,14.5], [4,122,86,2220,14],[4,116,90,2123,14]])
+            self.y = np.array([12, 45, 11, 90])
+        except IndexError:
+            print('Ensure predictor array lengths(x) are the same as response(y)')
+        
         self.test_train_split_regressor_result = 0
         
     def test_train_test_split_regressor(self):
@@ -66,13 +88,28 @@ class TestDataCollection_classifier(unittest.TestCase):
     assert_ytrain = 0
     assert_ytest = 0
     x, y = 0, 0
-    test_knn_classifier = 0
-    def setUpClass():
-        TestDataCollection_classifier.test_knn_classifier = knn.KNN("classifier", 3)
+    test_knn_classifier,k = 0,0
+    model_type = " "
+    @classmethod
+    def setUpClass(cls):
+        try:
+            TestDataCollection_classifier.test_knn_classifier = knn.KNN("classifier", 3)
+            if TestDataCollection_classifier.k < 0 and TestDataCollection_classifier.k > 10:
+                raise KnnValueError()
+            if TestDataCollection_classifier.model_type!= "regressor" or TestDataCollection_classifier.model_type!="classifier":
+                raise InvalidModel()
+        except KnnValueError:
+            print('K nearest neighbors is advisable to a maximum of 10 for statistical effiency')
+        except InvalidModel:
+            print('Invalid Model type, We are perfoming KNN Regression or KNN Classification')
+        
     def setUp(self):
         self.test_size = 0.4
-        self.x = np.array([[6.3,2.3,4.4,1.3],[6.8,3.2,5.9,2.3],[4.3,3,1.1,0.1],[3.5,3.1,1.0,0.5],[6.9,3.3,5.8,2.0]])
-        self.y = np.array(['Iris-versicolor', 'Iris-virginica', 'Iris-setosa', 'Iris-setosa','Iris-virginica'])
+        try:
+            self.x = np.array([[6.3,2.3,4.4,1.3],[6.8,3.2,5.9,2.3],[4.3,3,1.1,0.1],[3.5,3.1,1.0,0.5],[6.9,3.3,5.8,2.0]])
+            self.y = np.array(['Iris-versicolor', 'Iris-virginica', 'Iris-setosa', 'Iris-setosa','Iris-virginica'])
+        except IndexError:
+            print('Ensure predictor array lengths(x) are the same as response(y)')
         
     def test_train_test_split_classifier(self):
         
@@ -85,8 +122,11 @@ class TestDataCollection_classifier(unittest.TestCase):
         self.y_test = int(self.test_size * len(self.y))
         
         
-        
-        TestDataCollection_classifier.test_knn_classifier.train_test_split(0.4)
+        try:
+            TestDataCollection_classifier.test_knn_classifier.train_test_split(0.4)
+        except ValueError:
+            print('Enter a value between 0.1 to 0.9 as test-train split percentage')
+            
         self.assert_xtrain = len(TestDataCollection_classifier.test_knn_classifier.x_train)
         self.assert_xtest = len(TestDataCollection_classifier.test_knn_classifier.x_test)
         self.assert_ytrain = len(TestDataCollection_classifier.test_knn_classifier.y_train)
@@ -102,7 +142,8 @@ class TestDataCollection_classifier(unittest.TestCase):
     def tearDown(self):
         print(self.x_train, self.x_test, self.y_train, self.y_test)
         print(self.assert_xtrain,self.assert_xtest,self.assert_ytrain,self.assert_ytest)
-    def tearDownClass():
+    @classmethod
+    def tearDownClass(cls):
         print('Result successfull')
 
 unittest.main (argv =[''], verbosity=2, exit= False)  
