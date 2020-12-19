34cadc67fba2b9fedc417c5bbd1663323ee08712
Eric Phillips <ericphillips99@me.com>
2020-12-18 22:54:51 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version=0.1,
     packages=find_packages(exclude=['test*']),
     license='MIT',
-    description='Implementation of KNN model in Python. Includes built-in support for cross validation.',
+    description='KNN model in Python',
+    long_description='Implementation of K nearest neighbors model in Python. Supports built-in tuning of k hyper-parameter using k-fold cross validation.',
     url='https://github.com/PoojithaGowthaman/knn_integration',
     author='Poojitha Gowthaman & Eric Phillips',
     author_email='ericphillips99@me.com'
