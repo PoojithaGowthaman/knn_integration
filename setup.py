5fc14adbc1a03bae5f6b59220b5a29d8aeedff4d
Eric Phillips <ericphillips99@me.com>
2020-12-18 22:58:26 -0800

    Added long description for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ubc-knn',
-    version=0.1,
+    version=0.11,
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='KNN model in Python',
